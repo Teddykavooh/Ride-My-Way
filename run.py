d63d81276e72b973b8eb34ff94bc381f14c9d87d
Teddykavooh <teddykavooh@gmail.com>
2018-06-22 08:48:04 +0300

    Heroku Hosting

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 from app import create_app
+from werkzeug.contrib.fixers import ProxyFix
 
 
 config_name = os.getenv('APP_SETTINGS')
@@ -7,4 +8,6 @@ app = create_app(config_name=config_name)
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    # app.run()
